78ec5dd6accf8e20e6fa1469bffabac1c8f0aee7
Aitor Moreno Farré <amoreno@t4xi.com>
2019-11-02 19:39:26 +0100

    [TASK] - Update node version.

--- test/index.test.js
+++ test/index.test.js
@@ -16,11 +16,11 @@ const expect = chai.expect;
 const pathToReactGlobalConfiguration = argv.lib ? '../lib' : '../build';
 const pathToReactGlobalConfigurationReset = path.join(pathToReactGlobalConfiguration, 'reset');
 
-const reset = require(pathToReactGlobalConfigurationReset);
+const reset = require(pathToReactGlobalConfigurationReset).default;
 
 describe('react-global-configuration', () => {
     it('should not throw an error when set is called called prior', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         const configuration = {
             foo: {
@@ -32,7 +32,7 @@ describe('react-global-configuration', () => {
         config.get().should.deep.equal(configuration);
     });
     it('should throw an error if called more than once', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         config.set({ foo: 'bar' });
 
@@ -41,7 +41,7 @@ describe('react-global-configuration', () => {
         }).to.throw(Error);
     });
     it('should throw an error if called more than once, even if subsequent calls have freeze set to false', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         config.set({ foo: 'bar' });
 
@@ -50,7 +50,7 @@ describe('react-global-configuration', () => {
         }).to.throw(Error);
     });
     it('shouldn\'t throw an error if called more than once when the initial call had freeze set to false', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         const configuration = { baz: 'qux' };
         config.set({ foo: 'bar' }, { freeze: false });
@@ -59,21 +59,21 @@ describe('react-global-configuration', () => {
         config.get().should.deep.equal(configuration);
     });
     it('should throw an error if unrecognised options are passed', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         expect(() => {
             config.set({ foo: 'bar' }, { freeeze: false });
         }).to.throw(Error);
     });
     it('should throw an error if options are passed with unexpected types', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         expect(() => {
             config.set({ foo: 'bar' }, { assign: 'true' });
         }).to.throw(Error);
     });
     it('should extend the configuration, rather than replacing them if assign is set to true', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         config.set({ foo: 'bar' }, { freeze: false });
         config.set({ baz: 'qux' }, { assign: true });
@@ -82,7 +82,7 @@ describe('react-global-configuration', () => {
         config.get().should.deep.equal({ foo: { bar: 'baz' }, baz: 'qux' });
     });
     it('should return the string values', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         const configuration = { foo: 'bar' };
         config.set(configuration);
@@ -92,7 +92,7 @@ describe('react-global-configuration', () => {
         config.get(key).should.equal(configuration[key]);
     });
     it('should return the integer values', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         const configuration = {
             num: -1,
@@ -108,7 +108,7 @@ describe('react-global-configuration', () => {
         config.get('num_2').should.equal(2);
     });
     it('should return the boolean values', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         const configuration = {
             foo: false,
@@ -120,7 +120,7 @@ describe('react-global-configuration', () => {
         config.get('bar').should.equal(true);
     });
     it('should return null values', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         const configuration = {
             foo: null
@@ -132,7 +132,7 @@ describe('react-global-configuration', () => {
         should.equal(config.get(key), null);
     });
     it('should return object', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         const nestedConfiguration = {
             bar: 'baz'
@@ -145,7 +145,7 @@ describe('react-global-configuration', () => {
         config.get('foo').should.deep.equal(nestedConfiguration);
     });
     it('should return nested object value', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         const configuration = {
             foo: {
@@ -160,7 +160,7 @@ describe('react-global-configuration', () => {
         config.get(key).should.equal(configuration[keyParts[0]][keyParts[1]]);
     });
     it('should return array', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         const configuration = {
             foo: ['bar', 'baz']
@@ -172,7 +172,7 @@ describe('react-global-configuration', () => {
         config.get(key).should.equal(configuration[key]);
     });
     it('should return array value', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         const configuration = {
             foo: ['bar', 'baz']
@@ -185,7 +185,7 @@ describe('react-global-configuration', () => {
         config.get(key).should.equal(configuration[keyParts[0]][keyParts[1]]);
     });
     it('should return fallback value if config don\'t exist', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         const configuration = {
             foo: 'bar'
@@ -204,7 +204,7 @@ describe('react-global-configuration', () => {
         should.equal(config.get('bar', null), null);
     });
     it('should return serializes config', () => {
-        const config = require(pathToReactGlobalConfiguration);
+        const config = require(pathToReactGlobalConfiguration).default;
 
         const configuration = {
             foo: 'bar',
